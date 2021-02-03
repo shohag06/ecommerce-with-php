d91ee3411bbf010846301c923e2f0e0a12cb495b
shohag06 <shohag.cse06@gmail.com>
2021-02-02 23:15:07 -0800

    some php funtion added in top.inc.php page

--- top.inc.php
+++ top.inc.php
@@ -1,3 +1,22 @@
+
+<?php
+
+ require('connection.inc.php');
+ require('functions.inc.php');
+ 
+ if(isset($_SESSION['ADMIN_LOGIN']) && $_SESSION['ADMIN_LOGIN']!=''){
+	 
+ } else{
+	 header('location:login.php');
+		 die();
+	 
+ }
+      
+ 
+
+ ?>
+
+
 <!doctype html>
 <html class="no-js" lang="">
    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
@@ -23,13 +42,19 @@
                <ul class="nav navbar-nav">
                   <li class="menu-title">Menu</li>
                   <li class="menu-item-has-children dropdown">
-                     <a href="#" > Department Master</a>
+                     <a href="categories.php" >Categories Master</a>
                   </li>
                   <li class="menu-item-has-children dropdown">
-                     <a href="#" > Leave Type Master</a>
+                     <a href="product.php" >Product Master</a>
                   </li>
 				  <li class="menu-item-has-children dropdown">
-                     <a href="#" > Employee Master</a>
+                     <a href="#" >Order Master</a>
+                  </li>
+				   <li class="menu-item-has-children dropdown">
+                     <a href="#" >User Master</a>
+                  </li>
+				   <li class="menu-item-has-children dropdown">
+                     <a href="#" >Contact Us</a>
                   </li>
                </ul>
             </div>
